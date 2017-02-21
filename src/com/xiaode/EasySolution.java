e895c840985481246dd1c4d2d51e0182289052c9
xiaode <liyangde66@gmail.com>
2017-02-21 12:01:34 +0800

    haha

--- src/com/xiaode/EasySolution.java
+++ src/com/xiaode/EasySolution.java
@@ -1,5 +1,8 @@
 package com.xiaode;
 
+import java.util.*;
+//import java.util.List;
+
 /**
  * Created by xiaode on 11/9/16.
  */
@@ -61,5 +64,50 @@ public class EasySolution {
             z= z>>1;
         }
         return result;
+    }
+
+    /**
+     * 412. Fizz Buzz
+     * Write a program that outputs the string representation of numbers from 1 to n.
+     * But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output
+     * “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+     */
+
+    public List<String> fizzBuzz(int n){
+        String fizz = "Fizz";
+        String buzz = "Buzz";
+        String fizzBuzz = "FizzBuzz";
+        List<String> result = new ArrayList<>();
+        for(int i = 1; i <= n ; i++){
+            if (i % 3 == 0){
+                if(i % 5 == 0){
+                    result.add(fizzBuzz);
+                    continue;
+                } else {
+                    result.add(fizz);
+                    continue;
+                }
+            }
+            if (i % 5 == 0){
+                result.add(buzz);
+                continue;
+            }else{
+                result.add(String.valueOf(i));
+                continue;
+            }
+
+        }
+        return result;
+
+    }
+
+
+    /**
+     * 476. Number Complement
+     *
+     */
+    public int findComplement(int num){
+        int temp  = num & 0x1111;
+        return 0;//not finish
     }
 }
