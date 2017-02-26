328c1b02d888e3a1e4a791056997e18d6baeb4c4
xiaode <liyangde66@gmail.com>
2017-02-26 15:06:19 +0800

    1 easy

---
+++ src/com/xiaode/EasySolutions/ConrainsDuplicateII.java
@@ -0,0 +1,15 @@
+package com.xiaode.EasySolutions;
+
+/**
+ * Created by xiaode on 2/24/17.
+ */
+public class ConrainsDuplicateII {
+    /**
+     * 219.Contains Duplicate II
+     * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array
+     * such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+     */
+    public boolean containsNearbyDuplicate(int[] nums, int k){
+
+    }
+}
