13e325bbd7c311ee5f4a37bd3c0efbfac83cb1ac
xiaode <liyangde66@gmail.com>
2017-05-15 21:59:50 +0800

    1 easy dp

--- src/com/xiaode/Main.java
+++ src/com/xiaode/Main.java
@@ -10,46 +10,48 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-private static final Logger myLogger = Logger.getLogger("com.xiaode");
+    private static final Logger myLogger = Logger.getLogger("com.xiaode");
 
     public static void main(String[] args) {
 //	// write your code here
 //        MedianOfTwoSortedArrays ma = new MedianOfTwoSortedArrays();
 //        ma.findMedianSortedArray(new int[]{1,2},new int[]{3,4});
-        MyThread myThread =  new MyThread();
-        myThread.start();
-        try {
-            Thread.sleep(1000);
-            myThread.interrupt();
-            System.out.println(myThread.isInterrupted());
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//        try {
+//            Thread.sleep(1000);
+//            myThread.interrupt();
+//            System.out.println(myThread.isInterrupted());
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+       System.out.println(MaximumSubarray.maxSubArray(new int[]{2,-2,3,4,6,-5}));
 
+    }
+}
+
+class MyThread extends Thread {
+    private int count = 5;
+
+    @Override
+    synchronized public void run() {
+        super.run();
+        for (int i = 0; i < 50000000; i++) System.out.println(i);
 
     }
 
-
-class MyThread extends Thread{
-    private int count = 5;
-    @Override
-    synchronized public void run(){
-        super.run();
-        for (int i=0;i< 50000000;i++) System.out.println(i);
-
-        }
+    /**
+     * 如果使用最优的最大吞吐量负载均衡算法，按照最优模型多久能够处理完所有请求，单位毫秒。
+     *
+     * @return
+     */
+    static long doneTime(int maxQps, String[] rtList, int requestNum, int threadNum) {
+        //TODO
+        long totaltime = 0;
+        totaltime = (long) (requestNum / (maxQps * rtList.length)) * threadNum;
+        return totaltime;
+    }
 }
-        /**
-         * 如果使用最优的最大吞吐量负载均衡算法，按照最优模型多久能够处理完所有请求，单位毫秒。
-         * @return
-         */
-        static long doneTime(int maxQps,String[] rtList,int requestNum,int threadNum) {
-            //TODO
-            long  totaltime = 0;
-            totaltime = (long)(requestNum/(maxQps * rtList.length)) * threadNum;
-            return totaltime;
-        }
-
     
 //
 //输入:
