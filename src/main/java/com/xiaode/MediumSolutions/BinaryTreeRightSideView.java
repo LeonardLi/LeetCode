6a14e61bba7fadcbf051f4e0409434a2616b811b
xiaode <liyangde66@gmail.com>
2018-07-10 22:11:30 +0800

    7/10

---
+++ src/main/java/com/xiaode/MediumSolutions/BinaryTreeRightSideView.java
@@ -0,0 +1,29 @@
+package com.xiaode.MediumSolutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liyangde on Jul, 2018
+ *
+ * 199. Binary Tree Right Side View
+ *
+ * Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+ */
+public class BinaryTreeRightSideView {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){val = x;}
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        ArrayList<TreeNode> tree = new ArrayList<>();
+
+        TreeNode cur = root;
+
+        while(cu)
+
+        return null;
+    }
+}
--- src/main/java/com/xiaode/MediumSolutions/EscapeTheGhosts.java
+++ src/main/java/com/xiaode/MediumSolutions/EscapeTheGhosts.java
@@ -25,4 +25,14 @@ package com.xiaode.MediumSolutions;
  * The number of ghosts will not exceed 100.
  */
 public class EscapeTheGhosts {
+    public boolean escapeGhosts(int[][] ghosts, int[] target) {
+        int d = Math.abs(target[0]) + Math.abs(target[1]);
+
+        for (int[] ghost : ghosts) {
+            if (d >= Math.abs(ghost[0] - target[0]) + Math.abs(ghost[1] - target[1]))
+                return false;
+        }
+
+        return true;
+    }
 }
