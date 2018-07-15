9824f5208a6b8479b349abbaab829e3bce7b3d03
xiaode <liyangde66@gmail.com>
2018-07-15 09:09:22 +0800

    7/14

--- src/main/java/com/xiaode/MediumSolutions/BinaryTreeRightSideView.java
+++ src/main/java/com/xiaode/MediumSolutions/BinaryTreeRightSideView.java
@@ -1,7 +1,9 @@
 package com.xiaode.MediumSolutions;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by liyangde on Jul, 2018
@@ -18,17 +20,25 @@ public class BinaryTreeRightSideView {
         TreeNode(int x){val = x;}
     }
     public List<Integer> rightSideView(TreeNode root) {
-        ArrayList<TreeNode> tree = new ArrayList<>();
-        tree.add(new TreeNode(0));
+        List<Integer> rightView = new ArrayList<>();
+        Queue<TreeNode> nodes = new LinkedList<>();
 
-        TreeNode cur = root;
+        if (root != null) nodes.add(root);
 
+        TreeNode node;
+        while (!nodes.isEmpty()) {
+            rightView.add(nodes.peek().val);
+            int levelSize = nodes.size();
+            for (int i = 0; i < levelSize; i++) {
+                node = nodes.poll();
 
+                if (node.right != null) nodes.add(node.right);
 
-        return null;
+                if (node.left != null) nodes.add(node.left);
+            }
+        }
+
+        return rightView;
     }
 
-    private List<TreeNode> tranform(TreeNode root) {
-
-    }
 }
