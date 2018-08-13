8c8f7f2e62ba1d6ef566acdec4885de5ffcfb306
xiaode <liyangde66@gmail.com>
2018-08-13 22:24:55 +0800

    8/13

---
+++ src/main/java/com/xiaode/EasySolutions/ClimbingStairs.java
@@ -0,0 +1,23 @@
+package com.xiaode.EasySolutions;
+
+/**
+ * Created by liyangde on Aug, 2018
+ *
+ * 70. Climbing Stairs
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Note: Given n will be a positive integer.
+ */
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        int a = 1, b = 1;
+        while (n-- > 0)
+            a = (b += a) - a;
+        return a;
+    }
+
+}
---
+++ src/main/java/com/xiaode/EasySolutions/HouseRobber.java
@@ -0,0 +1,33 @@
+package com.xiaode.EasySolutions;
+
+/**
+ * Created by liyangde on Aug, 2018
+ *
+ * 198. House Robber
+ *
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ *
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+ *
+ */
+
+//Todo: fully understand DP algorithm
+
+public class HouseRobber {
+    public int rob(int[] nums) {
+        if(nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        result[1] = Math.max(nums[1], nums[0]);
+        for(int i = 2; i < nums.length; i++) {
+            if((nums[i] + result[i-2]) > result[i-1])
+                result[i] = nums[i] + result[i-2];
+            else
+                result[i] = result[i-1];
+        }
+        return result[nums.length - 1];
+    }
+}
