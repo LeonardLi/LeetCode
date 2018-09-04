487192c5f3bf6cf4e8fc95cac5fe4279dd4e8699
xiaode <liyangde66@gmail.com>
2018-09-03 21:34:45 -0700

    9/3

---
+++ src/main/java/com/xiaode/EasySolutions/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,7 @@
+package com.xiaode.EasySolutions;
+
+/**
+ * Created by liyangde on Sep, 2018
+ */
+public class MaximumProductOfThreeNumbers {
+}
---
+++ src/main/java/com/xiaode/MediumSolutions/MaximumProductSubarray.java
@@ -0,0 +1,7 @@
+package com.xiaode.MediumSolutions;
+
+/**
+ * Created by liyangde on Sep, 2018
+ */
+public class MaximumProductSubarray {
+}
