42777e7900e7f638f2b196e4c69763a2b24ca9ed
xiaode <liyangde66@gmail.com>
2018-09-12 22:33:25 -0700

    9/12

--- src/main/java/com/xiaode/BinaryTree/InOrder.java
+++ src/main/java/com/xiaode/BinaryTree/InOrder.java
@@ -1,7 +1,7 @@
 package com.xiaode.BinaryTree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 import com.xiaode.BinaryTree.TreeNode;
 
 public class InOrder {
@@ -14,8 +14,25 @@ public class InOrder {
         inOrderTraversal(root.right);
         return result;
     }
+
+
+
     //Iteratively
     public List<Integer> inOrderTraversal2(TreeNode root) {
-        return null;
+        if (root == null) return Collections.emptyList();
+
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> nodeStack = new ArrayDeque<>();
+        TreeNode current = root;
+
+        while (current != null || nodeStack.size() > 0) {
+            while (current != null) {
+                nodeStack.push(current);
+                current = current.left;
+            }
+            current = nodeStack.pop();
+            res.add(current.val);
+            current = current.right;
+        }
     }
 }
